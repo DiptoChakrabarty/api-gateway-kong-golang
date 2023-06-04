acf2f062d4c84abf1caba8849898bd98b9467699
DiptoChakrabarty <diptochuck123@gmail.com>
2023-06-04 13:22:28 +0530

    consideration of token generation

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	"net/http/httputil"
 	"net/url"
 
+	"api_microservice/controller"
+	"api_microservice/service"
+
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	jwtService      service.JWTService         = service.NewJWTService()
+	loginController controller.LoginController = controller.NewLoginController(jwtService)
 )
 
 func main() {
@@ -19,6 +27,18 @@ func main() {
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusAccepted, gin.H{"data": "App up and running"})
+	})
+
+	router.POST("/login", func(ctx *gin.Context) {
+		//token := controller.loginCon
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
 	})
 
 	router.Any("/api/*path", func(ctx *gin.Context) {
